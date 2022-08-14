8e5e02b23501217025116951ec72e405ddcfb191
KostasKv <kostas.kvietinskas@gmail.com>
2022-08-14 17:31:39 +0100

    move lang attribute to html tag, delete from head

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
-        <Head lang="en">
+      <Html lang="en">
+        <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
             <link href="https://fonts.googleapis.com/css2?family=Overpass:wght@400;700&display=swap" rel="stylesheet" /> 
